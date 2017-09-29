1f0c796062d1bb2a0fdc7c355c7b0cf79e263fbc
viktor.kovacs <viktor.kovacs@evosoft.com>
2017-09-29 09:50:36 +0200

    works with one book

--- src/main/java/HarryPotter.java
+++ src/main/java/HarryPotter.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class HarryPotter {
     public static double calculatePrice(List<Object> books) {
-        return 0;
+        return books.size() * 8;
     }
 }
--- src/test/java/HarryPotterTest.java
+++ src/test/java/HarryPotterTest.java
@@ -1,8 +1,15 @@
 import java.util.List;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class HarryPotterTest {
+    @Test
     public void testNoBooks() {
         Assert.assertEquals(0, HarryPotter.calculatePrice(List.of()), 0);
+    }
+
+    @Test
+    public void testOneBook() {
+        Assert.assertEquals(8, HarryPotter.calculatePrice(List.of(1)), 0);
     }
 }
